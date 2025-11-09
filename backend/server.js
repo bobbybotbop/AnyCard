b158c38f647ad557f54949a0386ad26ab612a6d9
Andrew Zhang <azhappy888@gmail.com>
2025-11-09 14:48:12 -0500

    Added basic boiler backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Define a basic route
+app.get("/", (req, res) => {
+  res.send("Hello from Express.js!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
