0484ebf5516bf90c9a3de2d64c89eeaab4d667eb
Andrew Zhang <azhappy888@gmail.com>
2025-11-14 15:23:07 -0500

    Done! Yippee

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,10 +11,6 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
