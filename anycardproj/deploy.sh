f53d19cebbde0404953db63125f2c7f6b8e3c4dd
Andrew Zhang <azhappy888@gmail.com>
2025-12-03 01:58:51 -0500

    😢

---
+++ anycardproj/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Exit if any command fails
+set -e
+
+echo "Deploying to Google Cloud Run..."
+
+# Make sure project is set
+gcloud config set project anycardbackend
+
+git checkout debugBackDeploy
+git pull origin debugBackDeploy
+
+# Build and deploy to Cloud Run
+gcloud run deploy anycard-server \
+  --source . \
+  --region us-central1 \
+  --allow-unauthenticated \
+  --min-instances=0 \
+  --cpu-throttling 
+  
+echo "Deployment complete!"
